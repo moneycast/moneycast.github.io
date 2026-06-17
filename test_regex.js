fa283dc597169f440cd35349e81572627d7a25bc
Daniel Ernesto Fernández González <denied911025@gmail.com>
2026-06-17 00:28:39 -0300

    Remove legacy code and tests related to card number extraction, OCR processing, and image resizing functionality

--- test_regex.js
+++
@@ -1,7 +0,0 @@
-const text = "9238 1299 7183 1286\r\nMARIA C. FIGUEREDO C.\r\nCUP\r\nVENCE: 07/32\r\nred\r\n";
-const match = text.match(/(?:\d[ \-]*?){15,19}/);
-console.log(match ? match[0].replace(/\D/g, '') : "No match");
-
-const text2 = "1234-5678-9012-3456 foo bar 123";
-const match2 = text2.match(/(?:\d[ \-]*?){15,19}/);
-console.log(match2 ? match2[0].replace(/\D/g, '') : "No match");
